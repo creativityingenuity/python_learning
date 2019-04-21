5fab2c68896dc823844f3253361095c83f19943f
vipgxs@gmail.com <vipgxs@gmail.com>
2019-04-21 22:59:35 +0800

    commit

---
+++ 4.21/01简单web服务器.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# @Time  : 2019/4/21 1:04
+
+# @Author : GOG
+
+# @Desc : ==============================================
+
+# Life is Short I Use Python!!!                      ===
+
+# If this runs wrong,don't ask me,I don't know why;  ===
+
+# If this runs right,thank god,and I don't know why. ===
+
+# Maybe the answer,my friend,is blowing in the wind. ===
+
+# ======================================================
+
+# @Project : python_learning
+
+# @FileName: 01简单web服务器.py
+
+# @Software: PyCharm
+import socket
+
+
+def main():
+	#1.创建套接字
+	socket.socket(socket.AF_INET,socket)
+	#2.绑定
+	#3.变为监听套接字
+	#4.等待新客户端的连接
+	#5.为这个客户端服务
+
+
+if __name__ == '__main__':
+    main()
